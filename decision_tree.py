e1ee9915b8c316f53cde4f44f90295ea243ea145
Nicole Story <nicole7story@gmail.com>
2019-02-28 10:30:57 -0600

    Added entropy and method stubs

--- decision_tree.py
+++ decision_tree.py
@@ -1,4 +1,6 @@
 from amino_acids import get_amino_acid
+from node import Node
+import math
 
 
 class DecisionTree(object):
@@ -8,8 +10,10 @@ class DecisionTree(object):
     sa = None
 
     def __init__(self, fasta, sa):
+        # TODO: Training data vs. test data
         self.fasta = fasta
         self.sa = sa
+        self.root = Node()
 
     def build_feature_matrix(self):
         for index in range(len(self.fasta)):
@@ -21,3 +25,58 @@ class DecisionTree(object):
 
             # Add the acid to the matrix
             self.matrix.append([])
+
+    def build_tree(self, current_node=None):
+        # Recursive 'build the tree' funct starting with root node
+        if current_node is None:
+            current_node = self.root
+
+        # Choose best attribute
+        # Create children nodes based on that attribute
+        # Recursive calls to those children nodes
+
+        pass
+
+    def compute_best_attribute(self, current_node):
+        # Based on current node, select
+        pass
+
+    def compute_entropy(self, outcome_list):
+        # Outcome list: the list of outcomes (y/n) for a given data set
+        # or attribute
+
+        # Calc number of each outcome
+        outcome_count = {}
+        for outcome in outcome_list:
+            if outcome not in outcome_count:
+                outcome_count[outcome] = 0
+            outcome_count[outcome] += 1
+
+        # Calculate the entropy (sum of p*log(p))
+        sum = 0
+        for outcome in outcome_count.keys():
+            probability = float(outcome_count[outcome]) / len(outcome_list)
+            sum -= probability * math.log(probability, 2)
+
+        return sum
+
+    def evaluate_model(self):
+        pass
+
+    def calculate_eval_metrics(self):
+        pass
+
+
+options = {
+    'hydrophobic': 2,
+    'polar': 2,
+    'charged': 2,
+    'positive': 2,
+    'negative': 2,
+    'small': 2,
+    'tiny': 2,
+    'aliphatic': 2,
+    'aromatic': 2,
+    'proline': 2,
+    'rsa-label': 2
+}
