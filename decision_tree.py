b6abacae523fa7a3f91faec4a76e2703498517e2
Nicole Story <nicole7story@gmail.com>
2019-02-27 20:07:48 -0600

    Decision Tree builds feature matrix

---
+++ decision_tree.py
@@ -0,0 +1,23 @@
+from amino_acids import get_amino_acid
+
+
+class DecesionTree(object):
+    root = None
+    feature_matrix = None
+    fasta = None
+    sa = None
+
+    def __init__(self, fasta, sa):
+        self.fasta = fasta
+        self.sa = sa
+
+    def build_feature_matrix(self):
+        for index in range(len(self.fasta)):
+            # Create the AA object
+            acid = get_amino_acid(self.fasta[index].upper())
+
+            # Add the RSA label
+            acid['rsa-label'] = self.sa[index]
+
+            # Add the acid to the matrix
+            self.matrix.append([])
